bba95249f76db3dd82bdbe2a643025465356ba61
Nathan <40770861+corndogtenshi@users.noreply.github.com>
2022-05-28 18:13:43 -0500

    Create responsive base UI for color-guessing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles/main.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <main>
+      <header className="header">
+        <h1>What is the RGB value of this color?</h1>
+        <div className="color-container">
+          <div style={{ backgroundColor: 'red' }}>
+
+          </div>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
-      </header>
-    </div>
+        <div className='color-guess-container'>
+          <input className='color-guess red' placeholder='Red'></input>
+          <input className='color-guess green' placeholder='Green'></input>
+          <input className='color-guess blue' placeholder='Blue'></input>
+        </div>
+        <button className='submit-button'>Submit</button>
+      </header >
+    </main >
   );
 }
 
