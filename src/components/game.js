e766f2dce27a8d99079b07f4628bed509a8f7777
Nathan <40770861+corndogtenshi@users.noreply.github.com>
2022-05-29 13:33:06 -0500

    Implemented guessing functionality (UI programming & console.log) and value validation

--- src/components/game.js
+++ src/components/game.js
@@ -4,12 +4,119 @@ import '../styles/main.css'
 export default class Game extends React.Component {
     constructor() {
         super()
+        this.state = { guessRed: 0, guessGreen: 0, guessBlue: 0 }
         var RandomNumber = function (minX, maxX) {
             return Math.floor((Math.random() * maxX - minX) + minX)
         }
         this.colorRed = RandomNumber(0, 255)
         this.colorGreen = RandomNumber(0, 255)
         this.colorBlue = RandomNumber(0, 255)
+
+        var setGuessRed = function (guess) {
+            this.guessRed = guess
+        }
+        var setGuessBlue = function (guess) {
+            this.guessBlue = guess
+        }
+        var setGuessGreen = function (guess) {
+            this.guessGreen = guess
+        }
+
+    }
+
+    Check = () => {
+        console.log(this.state.guessRed)
+        console.log(this.state.guessBlue)
+        console.log(this.state.guessGreen)
+        console.log("Guess submitted!")
+    }
+
+    handleChange = (e) => { //Event handler for changing color values
+        let val = Number(e.target.value)
+
+        if (isNaN(val)) { //If NaN
+            switch (e.target.id) { //Set to previous value
+                case 'red':
+                    val = this.state.guessRed
+                    e.target.value = val
+                    break;
+                case 'blue':
+                    val = this.state.guessBlue
+                    e.target.value = val
+                    break;
+                case 'green':
+                    val = this.state.guessGreen
+                    e.target.value = val
+                    break;
+                default:
+                    console.log("Error! Please try refreshing the page.")
+            }
+        }
+        else {
+            if (val > 255) { //If value over 255 (max value) 
+                switch (e.target.id) { //Set to previous value
+                    case 'red':
+                        val = this.state.guessRed
+                        e.target.value = val
+                        break;
+                    case 'blue':
+                        val = this.state.guessBlue
+                        e.target.value = val
+                        break;
+                    case 'green':
+                        val = this.state.guessGreen
+                        e.target.value = val
+                        break;
+                    default:
+                        console.log("Error! Please try refreshing the page.")
+                }
+            }
+            if (val < 0) { //If value under 0 (min value)
+                switch (e.target.id) { //Set to previous value
+                    case 'red':
+                        val = this.state.guessRed
+                        e.target.value = val
+                        break;
+                    case 'blue':
+                        val = this.state.guessBlue
+                        e.target.value = val
+                        break;
+                    case 'green':
+                        val = this.state.guessGreen
+                        e.target.value = val
+                        break;
+                    default:
+                        console.log("Error! Please try refreshing the page.")
+                }
+            }
+            switch (e.target.id) { //Otherwise, set to current value
+                case 'red':
+                    this.setState({ guessRed: val })
+                    break;
+                case 'blue':
+                    this.setState({ guessBlue: val })
+                    break;
+                case 'green':
+                    this.setState({ guessGreen: val })
+                    break;
+                default:
+                    console.log("Error! Please try refreshing the page.")
+            }
+        }
+
+        /*
+        console.log(val)
+        if (val > 255) {
+            e.target.value = 255
+        }
+        if (val > 255) {
+            e.target.value = 255
+        }
+        if (val < 0) {
+            e.target.value = 0
+        }
+        */
+
     }
 
 
@@ -31,11 +138,11 @@ export default class Game extends React.Component {
                     >
                     </a>
                     <div className='color-guess-container'>
-                        <input className='color-guess red' placeholder='Red'></input>
-                        <input className='color-guess green' placeholder='Green'></input>
-                        <input className='color-guess blue' placeholder='Blue'></input>
+                        <input id='red' onChange={this.handleChange} className='color-guess red' placeholder='0-255'></input>
+                        <input id='green' onChange={this.handleChange} className='color-guess green' placeholder='0-255'></input>
+                        <input id='blue' onChange={this.handleChange} className='color-guess blue' placeholder='0-255'></input>
                     </div>
-                    <button className='submit-button'>Submit</button>
+                    <button onClick={this.Check} className='submit-button'>Submit</button>
                 </header >
             </main >
         )
